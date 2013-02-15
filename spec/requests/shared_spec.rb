d66b9ba648ce4a42ec078053184f517d420970dc
Brett Weaver <brett@weav.net>
2013-02-15 12:24:53 -0800

    setting specs to pending for now

--- spec/requests/shared_spec.rb
+++ spec/requests/shared_spec.rb
@@ -2,17 +2,6 @@ require 'spec_helper'
 
 describe RubyTrelloCli::Requests::Shared do
 
-  before do 
-    @object = Object.new
-    @object.extend RubyTrelloCli::Requests::Shared
-  end
-  
-  it "should connect to trello" do
-    @object.stub :key   => 'dakey',
-                 :token => 'datoken'
+  it "should connect to trello"
 
-    Trello.should_receive(:configure).and_yield
-
-    @object.connect_to_trello
-  end
 end
