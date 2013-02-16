38a05932c5ac72c4e565737c2e9e895aa0bd65b7
Brett Weaver <brett@weav.net>
2013-02-16 11:39:06 -0800

    updated cmd logic

--- lib/ruby-trello-cli/cli/run.rb
+++ lib/ruby-trello-cli/cli/run.rb
@@ -3,22 +3,31 @@ module RubyTrelloCli
     class Run
       def run
         target = ARGV.shift
-        cmd    = ARGV.shift
+        cmd    = ARGV.shift.to_sym
 
         case target
         when 'card'
-          CLI::Card.new.send cmd
+          card = CLI::Card.new
+          card.send cmd if card.local_methods.include? cmd
         when 'list'
-          CLI::List.new.send cmd
+          list = CLI::List.new
+          list.send cmd if list.local_methods.include? cmd
         when '-v'
           puts RubyTrelloCli::VERSION
         else
           puts "Unkown target: '#{target}'." unless target == '-h'
-          puts "trello [card|list] [command] OPTIONS"
-          puts "Append -h for help on specific command."
+          puts "trello [#{targets.join('|')}] [command] OPTIONS"
+          puts "Append -h for help on specific target."
         end
-
       end
+
+      private
+
+      def targets
+        klasses = RubyTrelloCli::CLI.constants.reject { |c| c == :Run }
+        klasses.map { |k| k.to_s.downcase }
+      end
+
     end
   end
 end
