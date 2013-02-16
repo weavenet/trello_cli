b03bf36c9d842115fa38b787e7baa40de006bec0
Brett Weaver <brett@weav.net>
2013-02-16 09:39:58 -0800

    adding shared spec

--- spec/requests/shared_spec.rb
+++ spec/requests/shared_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 describe RubyTrelloCli::Requests::Shared do
 
-  it "should connect to trello"
+  before do
+    @object = Object.new
+    @object.extend RubyTrelloCli::Requests::Shared
+  end
+
+  it "should connect to trello" do
+    trello_configure_mock = mock 'trello_configure'
+    Trello.should_receive(:configure).and_yield trello_configure_mock
+    trello_configure_mock.should_receive(:developer_public_key=).
+                          with('key')
+    trello_configure_mock.should_receive(:member_token=).
+                          with('token')
+    @object.stub :key => 'key', :token => 'token'
+    @object.connect_to_trello
+  end
 
 end
